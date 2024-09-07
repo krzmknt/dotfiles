edc289305eb703a0c93efd299a9c19062e341e77
Kanta Kurzumi <krzmknt@gmail.com>
2024-09-08 03:37:09 +0900

    Introduce easy inline markdown preview

--- config/nvim/.config/nvim/lua/plugins.lua
+++ config/nvim/.config/nvim/lua/plugins.lua
@@ -490,7 +490,20 @@ return packer.startup(function(use)
     run = function() vim.fn["mkdp#util#install"]() end,
   })
 
+  use({
+    'MeanderingProgrammer/render-markdown.nvim',
+    after = { 'nvim-treesitter' },
+    requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+    -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+    -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+    config = function()
+      require('render-markdown').setup({})
+    end,
+  })
 
+  -----------------------------
+  -- Noice
+  --
   use({ "MunifTanjim/nui.nvim" })
   use({ "rcarriga/nvim-notify" })
   use({
@@ -507,6 +520,8 @@ return packer.startup(function(use)
       "rcarriga/nvim-notify",
     }
   })
+
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
